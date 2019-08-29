3f7d2b0b7c7de1c25068c7c15f228eb4955d0c68
vladislove <o4ennadoelip>
2019-08-29 03:17:38 +0300

    added edittext, button, portrait orientation, to main activity

--- app/src/main/java/com/uvn/ticker/MainActivity.kt
+++ app/src/main/java/com/uvn/ticker/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.uvn.ticker.tickerview.TickerScreenParams
 import com.uvn.ticker.tickerview.TickerViewActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val TAG_TICKER_PARAMS = "ticker_params"
 
@@ -14,14 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, TickerViewActivity::class.java).apply {
-            putExtra(
-                TAG_TICKER_PARAMS, TickerScreenParams(
-                    "Сдам квартиру у моря, цена договорная) ",
-                    textSize = 500f,
-                    textSpeed = 20f
-                )
-            )
-        })
+        btnNext.setOnClickListener {
+            if (etMessage.text.isNotEmpty()) {
+                startActivity(Intent(this, TickerViewActivity::class.java).apply {
+                    putExtra(
+                        TAG_TICKER_PARAMS, TickerScreenParams(
+                            etMessage.text.toString() + " ",
+                            textSize = 500f,
+                            textSpeed = 20f
+                        )
+                    )
+                })
+            }
+        }
     }
 }
