e267d4386748e1a77cf73a612fd1a7809bdad256
vladislove <o4ennadoelip>
2019-09-01 13:34:57 +0300

    added immersive mode to TickerViewActivity

--- app/src/main/java/com/uvn/ticker/ui/tickerview/TickerViewActivity.kt
+++ app/src/main/java/com/uvn/ticker/ui/tickerview/TickerViewActivity.kt
@@ -1,7 +1,8 @@
 package com.uvn.ticker.ui.tickerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.uvn.ticker.R
 import com.uvn.ticker.ui.TAG_TICKER_PARAMS
 import com.uvn.ticker.ui.tickerview.model.TickerParams
@@ -16,5 +17,26 @@ class TickerViewActivity : AppCompatActivity() {
         intent.extras?.getParcelable<TickerParams>(TAG_TICKER_PARAMS)?.let { params ->
             tickerView.initParams(params)
         }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+        else showSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    }
+
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
